e4e5111b2fb772e89a4804a7a375502ae9614bee
iArsene69 <mirzaprati@gmail.com>
2023-04-09 16:00:28 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const { Client, IntentsBitField } = require("discord.js");
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+client.on('ready', (c) => {
+    console.log(`${c.user.tag} is online!`);
+});
+
+client.on('messageCreate', (message) => {
+    if (message.author.bot) {
+        return;
+    }
+
+    if (message.content === 'test') {
+        message.reply('toast');
+    }
+});
+
+client.login(process.env.TOKEN);
