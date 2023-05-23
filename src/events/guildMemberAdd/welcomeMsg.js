aad17cf4769ed1c9f45af1bcc2e8ef11ab25bf0b
iArsene69 <mirzaprati@gmail.com>
2023-05-23 14:08:49 +0700

    fix-welcome-msg

--- src/events/guildMemberAdd/welcomeMsg.js
+++ src/events/guildMemberAdd/welcomeMsg.js
@@ -1,91 +1,95 @@
 const { Client, GuildMember, AttachmentBuilder } = require("discord.js");
-const Welcome = require('../../schema/welcome');
-const Canvas = require('canvas');
-const Discord = require('discord.js');
-
-
-
+const Welcome = require("../../schema/welcome");
+const { drawCard, LinearGradient } = require("discord-welcome-card");
 
 /**
- * 
- * @param {Client} client 
+ *
+ * @param {Client} client
  * @param {GuildMember} member
-*/
+ */
 
-const bg = "https://ibb.co/L0kXthr";
+// const bg = "https://ibb.co/L0kXthr";
 
-const dim = {
-    height: 675,
-    width: 1200,
-    margin: 50
-}
+// const dim = {
+//     height: 675,
+//     width: 1200,
+//     margin: 50
+// }
 
-const av = {
-    size: 256,
-    x: 480,
-    y: 170
-}
+// const av = {
+//     size: 256,
+//     x: 480,
+//     y: 170
+// }
 
+// const generateImg = async (member) => {
 
-const generateImg = async (member) => {
-    
-    let username = member.user.username;
-    let discriminator = member.user.discriminator;
-    let avatarUrl = member.user.displayAvatarURL({
-        format: "jpg",
-        dynamic: false,
-        size: av.size
-    });
-    const canvas = Canvas.createCanvas(dim.width, dim.height);
-    const context = canvas.getContext('2d');
+//     let username = member.user.username;
+//     let discriminator = member.user.discriminator;
+//     let avatarUrl = member.user.displayAvatarURL({
+//         format: "png",
+//         dynamic: false,
+//         size: av.size
+//     });
+//     const canvas = Canvas.createCanvas(dim.width, dim.height);
+//     const context = canvas.getContext('2d');
 
-    const backgroundImg = await Canvas.loadImage(bg);
-    context.drawImage(backgroundImg, 0, 0);
+//     const backgroundImg = await Canvas.loadImage(bg);
+//     context.drawImage(backgroundImg, 0, 0);
 
-    context.fillStyle = "rgba(0,0,0,0)"
-    context.fillRect(dim.margin, dim.margin, dim.width - 2 * dim.margin, dim.height - 2 * dim.margin);
+//     context.fillStyle = "rgba(0,0,0,0)"
+//     context.fillRect(dim.margin, dim.margin, dim.width - 2 * dim.margin, dim.height - 2 * dim.margin);
 
-    const avImg = await Canvas.loadImage(avatarUrl);
-    context.save();
+//     const avImg = await Canvas.loadImage(avatarUrl);
+//     context.save();
 
-    context.beginPath();
-    context.arc(av.x + av.size / 2, av.y + av.size / 2, av.size / 2, 0, Math.PI * 2, true);
-    context.closePath();
-    context.clip();
+//     context.beginPath();
+//     context.arc(av.x + av.size / 2, av.y + av.size / 2, av.size / 2, 0, Math.PI * 2, true);
+//     context.closePath();
+//     context.clip();
 
-    context.drawImage(avImg, av.x, av.y);
-    context.restore();
+//     context.drawImage(avImg, av.x, av.y);
+//     context.restore();
 
-    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome.png');
-    return attachment;
-}
-
+//     const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome.png');
+//     return attachment;
+// }
 
 module.exports = async (client, member) => {
-    try {
-        let guild = member.guild;
-        if (!guild) return;
+  try {
+    const welcomeCard = await drawCard({
+      theme: "dark",
+      text: {
+        title: `Welcome to ${member.guild.name}`,
+        text: member.user.tag,
+        subtitle: "Please read and follow the rules",
+        color: `#88f`,
+      },
+      avatar: {
+        image: member.user.displayAvatarURL({ extension: "png" }),
+        outlineWidth: 6,
+        outlineColor: new LinearGradient([0, "#33f"], [1, "#f33"]),
+      },
+      blur: 2,
+      border: true,
+      rounded: true,
+    });
 
-        const welcome = await Welcome.findOne({
-            guildId: guild.id
-        });
-        if (!welcome) return;
+    let guild = member.guild;
+    if (!guild) return;
 
-        const channel = client.channels.cache.get(welcome.channelId);
+    const welcome = await Welcome.findOne({
+      guildId: guild.id,
+    });
+    if (!welcome) return;
 
-        const img = await generateImg(member);
+    const channel = client.channels.cache.get(welcome.channelId);
 
-
-
-        await channel.send(
-            {
-                content: `Hello`,
-                files: [img],
-            },
-        );
-
-    } catch (error) {
-        console.log(`There was error sending greeting card ${error}`);
-    }
-}
-
+    await channel.send({
+      content: `Welcome ${member.user.username}`,
+      files: [welcomeCard],
+    });
+  } catch (error) {
+    console.log(`There was error sending greeting card ${error}`);
+  }
+};
